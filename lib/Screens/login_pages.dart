6b7724c2ae291fc5e13dbb0d74b47ed63b376a89
georgediyeve <83222104+georgediyeve@users.noreply.github.com>
2023-05-18 16:49:01 +0200

    login_pages.dart

--- lib/Screens/login_pages.dart
+++ lib/Screens/login_pages.dart
@@ -1,113 +1,119 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
-  @override
-  State<LoginPage> createState() => _LoginPageState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _LoginPageState extends State<LoginPage> {
-//A controller for an editable text field.
-//Whenever the user modifies a text field with an associated [TextEditingController], the text field updates [value] and the controller notifies its listeners.
-  TextEditingController usernameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-        begin: Alignment.topRight,
-        end: Alignment.bottomLeft,
-        colors: [
-          Colors.blueGrey,
-          Colors.teal,
-        ],
-      )),
-      child: Scaffold(
-        backgroundColor: Colors.amber,
-        body: _page(),
-      ),
-    );
-  }
-
-  Widget _page() {
-    return Padding(
-      padding: const EdgeInsets.all(32.0),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _icon(),
-            const SizedBox(height: 50),
-            _inputField("Username", usernameController),
-            const SizedBox(height: 20),
-            _inputField("Password", passwordController, isPassword: true),
-            const SizedBox(height: 50),
-            _loginBtn(),
-            const SizedBox(height: 20),
-            _extraText(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _icon() {
-    return Container(
-      decoration: BoxDecoration(
-          border: Border.all(color: Colors.white, width: 2),
-          shape: BoxShape.circle),
-      child: const Icon(Icons.person, color: Colors.white, size: 120),
-    );
-  }
-
-  Widget _inputField(String hintText, TextEditingController controller,
-      {isPassword = false}) {
-    var border = OutlineInputBorder(
-        borderRadius: BorderRadius.circular(18),
-        borderSide: const BorderSide(color: Colors.white));
-
-    return TextField(
-      style: const TextStyle(color: Colors.white),
-      controller: controller,
-      decoration: InputDecoration(
-        hintText: hintText,
-        hintStyle: const TextStyle(color: Colors.white),
-        enabledBorder: border,
-        focusedBorder: border,
-      ),
-      obscureText: isPassword,
-    );
-  }
-
-  Widget _loginBtn() {
-    return ElevatedButton(
-      onPressed: () {
-        debugPrint("Username : " + usernameController.text);
-        debugPrint("Password : " + passwordController.text);
-      },
-      child: const SizedBox(
-          width: double.infinity,
-          child: Text(
-            "Sign in ",
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 20),
-          )),
-      style: ElevatedButton.styleFrom(
-        shape: const StadiumBorder(),
-        backgroundColor: const Color.fromARGB(
-            255, 133, 96, 96), //primary: Color.fromARGB(255, 133, 96, 96),
-        foregroundColor: Colors.amber, //onPrimary: Colors.amber,
-        padding: const EdgeInsets.symmetric(vertical: 16),
-      ),
-    );
-  }
-
-  Widget _extraText() {
-    return const Text(
-      "Unable to access your account?",
-      textAlign: TextAlign.center,
-      style: TextStyle(fontSize: 17, color: Colors.amber),
+    return MaterialApp(
+     home: HomePage(),
     );
   }
 }
+
+/*
+TODO list:
+ 1. Create the main layout of the app
+
+*/
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: LoginScreen(),
+    );
+  }
+}
+
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children:  [
+           const Text(
+                "Word Wise",
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize:42.0,
+                  fontWeight: FontWeight.bold,
+                ),
+            ),
+            const Text("Login to the App",
+              style: TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 34.0,
+                  fontWeight: FontWeight.bold,
+
+              ),
+            ),
+            const SizedBox(
+              height: 44.0,
+            ),
+            const TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: "Email required",
+                    prefixIcon: Icon(Icons.mail, color:Colors.amberAccent),
+              ),
+            ),
+            const SizedBox(
+              height: 26.0,
+            ),
+            const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Password",
+                prefixIcon: Icon(Icons.lock, color: Colors.amber,)
+              ),
+            ),
+            const SizedBox(height: 13.0,),
+            const Text("Is your password lost in the Matrix?",
+                style:TextStyle(color: Colors.deepOrange)
+            ),
+            const SizedBox(height: 88.0,),
+            Container(
+              width: double.infinity,
+              child: RawMaterialButton(
+                fillColor:Color(0xFFFE0026),
+                elevation:0.0,
+                padding: EdgeInsets.symmetric(vertical: 20.0),
+                shape: RoundedRectangleBorder(
+                    borderRadius:BorderRadius.circular(12.0)),
+                onPressed: () {},
+                child: Text("LogIn", style: ,),
+              ),
+            ),
+          ],
+        ),
+
+    );
+  }
+}
+
+
+
+
