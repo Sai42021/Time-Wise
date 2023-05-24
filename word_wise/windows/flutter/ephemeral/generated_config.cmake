cc92b3e755b2eaa506bf7f48df69c49f80ef0ac5
Abisai Sem <83239286+Sai42021@users.noreply.github.com>
2023-05-24 02:34:02 +0200

    Add files via upload

---
+++ word_wise/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\SAI 420\\Documents\\xindo Documents\\Courses\\Mobile Application Development(MAP711S)\\Assignment\\word_wise" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter src\\flutter"
+  "PROJECT_DIR=C:\\Users\\SAI 420\\Documents\\xindo Documents\\Courses\\Mobile Application Development(MAP711S)\\Assignment\\word_wise"
+  "FLUTTER_ROOT=C:\\flutter src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\SAI 420\\Documents\\xindo Documents\\Courses\\Mobile Application Development(MAP711S)\\Assignment\\word_wise\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\SAI 420\\Documents\\xindo Documents\\Courses\\Mobile Application Development(MAP711S)\\Assignment\\word_wise"
+  "FLUTTER_TARGET=C:\\Users\\SAI 420\\Documents\\xindo Documents\\Courses\\Mobile Application Development(MAP711S)\\Assignment\\word_wise\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\SAI 420\\Documents\\xindo Documents\\Courses\\Mobile Application Development(MAP711S)\\Assignment\\word_wise\\.dart_tool\\package_config.json"
+)
